955c89f8b3267eae0e11c13505cf6d2fe54a1812
Tanveer Singh <tanveerbadhan@gmail.com>
2025-01-06 20:20:45 -0500

    Initial Commit

---
+++ IntroSwift/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  IntroSwift
+//
+//  Created by Tanveer Singh on 2025-01-06.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var msg = "My first message in swift";
+
+print(msg);
+
+// String interpolation in swift is done by using /()
+print("Type of msg is : \(type(of: msg))");
+
+var v1: Int = 10;
+var v2: Double = 10.3;
+var v3: String = "I am a string";
+var v4: Bool = true;
+var v5: Float = 12.34;
+
+print("Value of v1 is : \(v1)");
+print("Value of v2 is: \(v2)");
+print("Value of v3 is: \(v3)");
+print("Value of v4 is: \(v4)");
+print("Value of v5 is: \(v5)");
+
+
